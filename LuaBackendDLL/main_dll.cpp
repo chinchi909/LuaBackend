7a028d5a30bb088fbbd527d87939bbd6f524cf24
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2021-09-20 23:09:05 -0700

    Update version string

--- LuaBackendDLL/main_dll.cpp
+++ LuaBackendDLL/main_dll.cpp
@@ -103,7 +103,7 @@ extern "C"
 		ShowWindow(GetConsoleWindow(), SW_HIDE);
 
 		cout << "======================================" << "\n";
-		cout << "======= LuaBackendHook | v1.1.5 ======" << "\n";
+		cout << "======= LuaBackendHook | v1.2.0 ======" << "\n";
 		cout << "====== Copyright 2021 - TopazTK ======" << "\n";
 		cout << "======================================" << "\n";
 		cout << "=== Compatible with LuaEngine v5.0 ===" << "\n";
